ef294b5207f7e27979de5972fae11262fde7f41e
Basil-Pesto-Pizza <gjhsh9077@naver.com>
2024-09-25 17:37:57 +0900

    <FastAPI 베이스 라인2> 1. 파일구조 변경했습니다. 2. 수행방법 구체적으로 작성했습니다. 3. 발생한 오류 정리했습니다.

--- main.py
+++ main.py
@@ -16,17 +16,11 @@ def read_root():
 # 	alt + f12 ->
 # 	pip install fastapi
 #   	pip install uvicorn
-#
 # 2. main.py 파일 생성 후 코드 작성
-#
 # 3. _init_.py 파일 생성
-#
 # 4. FastAPI 서버 실행 (테스트)
-# 터미널에 다음 입력
-# uvicorn main:app
-#
+# 터미널에 다음 입력 : uvicorn main:app
 # 5. http://localhost:8000/ 접속
-#
 # 6. FastAPI - Swagger UI
 # Swagger UI 라고 하는 이 화면은 FastAPI에서 각 API의 기능 테스트에 상당히 많이 쓰인다.
 # hello world 의 우측을 클릭하여 펼친 후 Try it out > Execute 를 클릭하면 아래와 같은 화면을 확인 할 수 있다.
@@ -39,7 +33,6 @@ def read_root():
 # - 해결 방법 : 파일 경로 수정
 #     (C:\Users\KFQ\Desktop\git\BACKEND\__init__.py)
 #     (C:\Users\KFQ\Desktop\git\BACKEND\main.py)
-
 # 2. Error while finding module specification for 'main.py'
 #   (ModuleNotFoundError: __path__ attribute not found on 'main'
 #   while trying to find 'main.py'). Try using 'main' instead of 'main.py' as the module name.
