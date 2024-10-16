7eafb1dba7d19692964453e0d0c0d05254764d00
Basil-Pesto-Pizza <gjhsh9077@naver.com>
2024-10-16 15:09:55 +0900

    <Fastapi router 구성4> - CORS 설정에서 allow_origins 수정: allow_origins=["*"]

--- fastapi/main.py
+++ fastapi/main.py
@@ -9,10 +9,11 @@ import importlib
 
 app = FastAPI()
 
+
 # CORS 설정
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:8000"],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
