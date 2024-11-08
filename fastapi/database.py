2bced947d827e3d543f087cb46e6675a44dbdb87
Basil-Pesto-Pizza <gjhsh9077@naver.com>
2024-11-09 01:59:34 +0900

    일단 프레스랑 용접 기능 만들었음

--- fastapi/database.py
+++ fastapi/database.py
@@ -2,27 +2,27 @@ import aiomysql
 
 async def get_db_connection():
     return await aiomysql.connect(
-        host='opyter.iptime.org',
+        host='ec2-100-24-7-128.compute-1.amazonaws.com',
         user='bigdata_busan_3',
-        password='busan12345678*',
+        password='busan12345678',
         db='web',
         port=3306
     )
 
 async def get_db_press_connection():
     return await aiomysql.connect(
-        host='opyter.iptime.org',
+        host='ec2-100-24-7-128.compute-1.amazonaws.com',
         user='bigdata_busan_3',
-        password='busan12345678*',
+        password='busan12345678',
         db='press',
         port=3306
     )
 
 async def get_db_welding_connection():
     return await aiomysql.connect(
-        host='opyter.iptime.org',
+        host='ec2-100-24-7-128.compute-1.amazonaws.com',
         user='bigdata_busan_3',
-        password='busan12345678*',
+        password='busan12345678',
         db='welding',
         port=3306
     )
